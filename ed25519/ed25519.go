67f23a7af61a808abcb8d474ae71d566e76303ba
sanrentai <sanrentai@163.com>
2020-05-26 15:40:38 +0800

    add GenerateKeyPemFile method

--- ed25519/ed25519.go
+++ ed25519/ed25519.go
@@ -2,6 +2,7 @@ package ed25519
 
 import (
 	"crypto/ed25519"
+	"encoding/pem"
 	"os"
 )
 
@@ -32,6 +33,47 @@ func GenerateKeyFile(privateKeyFile, publickKeyFile string) error {
 
 	_, err = pubfile.Write(pub)
 	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GenerateKeyPemFile(privateKeyFile, publickKeyFile string) error {
+
+	pub, pri, err := ed25519.GenerateKey(nil)
+
+	if err != nil {
+		return err
+	}
+
+	prifile, err := os.Create(privateKeyFile)
+	if err != nil {
+		return err
+	}
+	defer prifile.Close()
+
+	priblock := pem.Block{
+		Type:  "ED25519 PRIVATE KEY",
+		Bytes: pri,
+	}
+
+	if err = pem.Encode(prifile, &priblock); err != nil {
+		return err
+	}
+
+	pubfile, err := os.Create(publickKeyFile)
+	if err != nil {
+		return err
+	}
+	defer pubfile.Close()
+
+	pubblock := pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: pub,
+	}
+
+	if err = pem.Encode(pubfile, &pubblock); err != nil {
 		return err
 	}
 
--- main.go
+++ main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/sanrentai/crypto/ed25519"
 )
 
 func main() {
-	err := ed25519.GenerateKeyFile("pri.key", "pub.key")
-	if err != nil {
-		fmt.Println(err)
+	if err := ed25519.GenerateKeyFile("pri.key", "pub.key"); err != nil {
+		panic(err)
+	}
+
+	if err := ed25519.GenerateKeyPemFile("pri.pem", "pub.pem"); err != nil {
+		panic(err)
 	}
 
 }
